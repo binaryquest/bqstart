4e9ba1f3d2fc7863c2ea6241ff39d5d34ee359cd
Sazzad Hossain <sazzad@sumon.net>
2022-01-04 22:19:39 +0600

    update base model

--- aspnet/src/bqStart/bqStart.Web/ClientApp/projects/bq-start-prime/src/lib/models/base-model.ts
+++ aspnet/src/bqStart/bqStart.Web/ClientApp/projects/bq-start-prime/src/lib/models/base-model.ts
@@ -1,44 +1,15 @@
 export class BaseEntity<T>{
   Id: T;
-  CreatedOn: Date;
-  ModifiedOn: Date;
-  CreatedBy: string;
-  ModifiedBy: string;
-  RecordState: string;
-
-  getRecordState(): RecordState {
-    if (this.RecordState === "Inserted") {
-      return RecordState.Inserted;
-    } else if (this.RecordState === "Modified") {
-      return RecordState.Modified;
-    } else if (this.RecordState === "Deleted") {
-      return RecordState.Deleted;
-    } else {
-      return RecordState.Unchanged;
-    }
-  }
-
-  setRecordState(rs: RecordState) {
-    switch (rs) {
-      case RecordState.Unchanged:
-        this.RecordState = "Unchanged";
-        break;
-      case RecordState.Inserted:
-        this.RecordState = "Inserted";
-        break;
-      case RecordState.Modified:
-        this.RecordState = "Modified";
-        break;
-      case RecordState.Deleted:
-        this.RecordState = "Deleted";
-        break;
-    }
-  }
+  CreatedOn: Date|null;
+  ModifiedOn: Date|null;
+  CreatedBy: string|null;
+  ModifiedBy: string|null;
+  RecordState: RecordState|null;
 }
 
 export enum RecordState {
-  Unchanged = 0,
-  Inserted = 1,
-  Modified = 2,
-  Deleted = 3
+  Unchanged = "Unchanged",
+  Inserted = "Inserted",
+  Modified = "Modified",
+  Deleted = "Deleted"
 }
