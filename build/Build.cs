4c2b3ae8f370bbc1cc98be592d74b12006c656a0
Sazzad Hossain <sazzad@sumon.net>
2022-01-08 12:24:45 +0600

    nuke

---
+++ build/Build.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[ShutdownDotNetAfterServerBuild]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter] string NugetApiUrl = "https://api.nuget.org/v3/index.json"; //default
+    [Parameter] string NugetApiKey;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    AbsolutePath SourceDirectory => RootDirectory / "aspnet" / "src" / "bqStart";
+    AbsolutePath TestsDirectory => RootDirectory / "aspnet" / "src" / "bqStart";
+    AbsolutePath OutputDirectory => RootDirectory / "aspnet" / "output";
+    AbsolutePath PackagesDirectory => RootDirectory / "aspnet" / "output" / "packages";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)        
+        .Executes(() =>
+        {
+            Logger.Info($"Root Folder {RootDirectory}");
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Produces(PackagesDirectory / "*.nupkg")
+        .Executes(() =>
+        {            
+            DotNetPack(s => s
+              .SetProject(Solution.GetProject("BinaryQuest.Framework.Core"))
+              .SetConfiguration(Configuration)
+              .SetVersion(GitVersion.NuGetVersionV2)
+              .EnableNoBuild()
+              .EnableNoRestore()              
+              .SetNoDependencies(true)
+              .SetOutputDirectory(PackagesDirectory));
+
+            DotNetPack(s => s
+              .SetProject(Solution.GetProject("BinaryQuest.Framework.Identity.UI"))
+              .SetConfiguration(Configuration)
+              .SetVersion(GitVersion.NuGetVersionV2)
+              .EnableNoBuild()
+              .EnableNoRestore()
+              .SetNoDependencies(true)
+              .SetOutputDirectory(PackagesDirectory));
+        });
+
+    Target Push => _ => _
+       .DependsOn(Pack)
+       .Requires(() => NugetApiUrl)
+       .Requires(() => NugetApiKey)
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .Executes(() =>
+       {
+           GlobFiles(PackagesDirectory, "*.nupkg")
+               .NotEmpty()
+               .Where(x => !x.EndsWith("symbols.nupkg"))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(NugetApiUrl)
+                       .SetApiKey(NugetApiKey)
+                   );
+               });
+       });
+}
