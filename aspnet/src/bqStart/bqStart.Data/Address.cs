e0f973d66b90ea25f8a4e2ebe0261186a496fa10
Sazzad Hossain <sazzad@sumon.net>
2023-09-27 15:58:30 +0600

    fix default value column meta data

--- aspnet/src/bqStart/bqStart.Data/Address.cs
+++ aspnet/src/bqStart/bqStart.Data/Address.cs
@@ -1,6 +1,7 @@
 ﻿using BinaryQuest.Framework.Core.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -16,6 +17,10 @@ namespace bqStart.Data
 
         [StringLength(500)]
         public string? City { get; set; }
+
+        [StringLength(500)]
+        [DefaultValue("N/A")]
+        public string? Country { get; set; }
 
         [InverseProperty(nameof(Department.AddressNavigation))]
         public virtual ICollection<Department>? DepartmentAddressNavigations { get; set; }
---
+++ aspnet/src/bqStart/bqStart.Data/Migrations/20230927081005_address03.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bqStart.Data.Migrations
+{
+    public partial class address03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(500)",
+                defaultValue: "N/A",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Addresses");
+        }
+    }
+}
